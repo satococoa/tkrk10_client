75d3bd547c9f4233ddc62659a73b76fc146108d3
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-01-12 17:53:39 +0900

    Blank view controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(App.bounds)
+    controller = UIViewController.new
+    controller.view.backgroundColor = UIColor.whiteColor
+    @window.rootViewController = controller
+    @window.makeKeyAndVisible
     true
   end
 end
